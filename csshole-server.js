78c33c9965c1eeaa24d0aaaf2f4548b6af9ae0f7
triplelog <robmwilc@gmail.com>
2020-04-17 15:59:39 -0400

    First

---
+++ csshole-server.js
@@ -0,0 +1,57 @@
+
+const https = require('https');
+var fs = require("fs");
+
+var qs = require('querystring');
+const { exec } = require('child_process');
+const bodyParser = require('body-parser');
+var nunjucks = require('nunjucks');
+var crypto = require("crypto");
+const options = {
+  key: fs.readFileSync('/etc/letsencrypt/live/sudoubleku.com/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/sudoubleku.com/fullchain.pem')
+};
+const { PerformanceObserver, performance } = require('perf_hooks');
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/triplelog', {useNewUrlParser: true});
+const User = require('./models/user');
+const UserData = require('./models/userdata');
+
+var express = require('express');
+
+
+
+var app = express();
+
+
+
+app.use('/',express.static('static'));
+
+app.get('/', 
+	
+	function(req, res) {
+		res.write(nunjucks.render('templates/csshole.html',{
+		
+		}));
+		res.end();
+	}
+);
+app.get('/index.html', 
+	
+	function(req, res) {
+		res.write(nunjucks.render('templates/csshole.html',{
+		
+		}));
+		res.end();
+	}
+);
+
+
+
+
+const server1 = https.createServer(options, app);
+
+server1.listen(12312);
+
+
