a6bcc4be5656d2f10ee351863881437fd838baa2
triplelog <robmwilc@gmail.com>
2020-05-21 23:12:40 -0400

    First

--- graphics/logo.js
+++ graphics/logo.js
@@ -2,32 +2,32 @@
 const OpenSimplexNoise = require('open-simplex-noise');
 const noise = new OpenSimplexNoise(40);
 
-export function drawFlower(ctx: CanvasRenderingContext2D,
-                           circle: {x: number, y: number, radius: number},
+function drawFlower(circle: {x: number, y: number, radius: number},
                            frequency: number = 2.0, magnitude: number = 0.5,
                            independence: number = 0.1, spacing: number = 0.01,
                            count: number = 300): void {
     // adjust the radius so will have roughly the same size irregardless of magnitude
     let current = {...circle};
     current.radius /= (magnitude + 1);
+    var paths = [];
     for (let i = 0; i < count; ++i) {
         // draw a circle, the final parameter controlling how similar it is to
         // other circles in this image
-        drawDeformedCircle(ctx, current,
+        drawDeformedCircle(current,
                            frequency, magnitude,
                            i * independence);
 
         // shrink the radius of the next circle
         current.radius *= (1 - spacing);
     }
+    console.log(paths);
 }
 
-export function drawDeformedCircle(ctx: CanvasRenderingContext2D,
-                                   circle: {x: number, y: number, radius: number},
+function drawDeformedCircle( circle: {x: number, y: number, radius: number},
                                    frequency: number,
                                    magnitude: number,
                                    seed: number = 0): void {
-        ctx.beginPath();
+        var path = 'M';
 
         // Sample points evenly around the circle
         const samples = Math.floor(4 * circle.radius + 20);
@@ -45,9 +45,10 @@ export function drawDeformedCircle(ctx: CanvasRenderingContext2D,
             const radius = circle.radius * (1 + magnitude * deformation);
 
             // Extend the circle to this deformed radius
-            ctx.lineTo(circle.x + radius * x,
-                       circle.y + radius * y);
+            path += (circle.x + radius * x) + ','+(circle.y + radius * y)+' ';
         }
-        ctx.fill();
-        ctx.stroke();
+        path += 'Z';
+        paths.push(path);
 }
+
+drawFlower({x:0,y:0,radius:100},2.0,0.5,0.1,0.01,7);
