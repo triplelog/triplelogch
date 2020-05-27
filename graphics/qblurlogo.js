0858e0b4e7148f179b5505f433aaab88ccb08c56
triplelog <robmwilc@gmail.com>
2020-05-26 23:26:30 -0400

    First

--- graphics/qblurlogo.js
+++ graphics/qblurlogo.js
@@ -20,7 +20,7 @@ function drawOvals(circle,frequency, magnitude,independence, spacing,count) {
     for (var i=0;i<paths.length;i++){
     	h = 10 + noise2D(.9-i/paths.length*.8,.1+i/paths.length*.8)*340;
     	s = (35 + noise2D(.1+i/paths.length*.8,.9-i/paths.length*.8)*20)+'%';
-    	l = (25+Math.min(i%11,10-(i%11))*9)+'%';
+    	l = (25+Math.min(i%17,16-(i%17))*6)+'%';
     	if (i == paths.length-1){
     		svg += '<path fill="white" stroke="black" d="'+paths[i]+'" />';
     	}
@@ -122,7 +122,7 @@ svg += line2;*/
 
 noise = OpenSimplexNoise.makeNoise3D(Date.now());
 noise2D = OpenSimplexNoise.makeNoise2D(Date.now());
-var end1 = drawOvals({x:250,y:250,radius:100},2.0,0.033,0.09,.3,80);
+var end1 = drawOvals({x:250,y:250,radius:100},2.0,0.033,0.09,.33,80);
 svg += end1;
 /*
 noise = OpenSimplexNoise.makeNoise3D(Date.now());
