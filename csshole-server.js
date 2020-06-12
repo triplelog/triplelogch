591989c530eee29e7308307154d696b7f3f884cd
triplelog <robmwilc@gmail.com>
2020-06-12 14:04:00 -0400

    First

--- csshole-server.js
+++ csshole-server.js
@@ -727,13 +727,13 @@ app.get('/css/sortablePitchers.css',
 	
 	function(req, res) {
 		console.log('getting css from server');
-		fs.readFile("/static/css/sortableP.css.gz", 'utf8', function(err, fileData) {
+		fs.readFile("static/css/sortableP.css.gz", 'utf8', function(err, fileData) {
 			if (err){
 				console.log('err',performance.now());
 			}
 			else {
 				console.log('found css',performance.now());
-				var raw = fs.createReadStream("/static/css/sortableP.css.gz");
+				var raw = fs.createReadStream("static/css/sortableP.css.gz");
 				res.writeHead(200, {'Content-Type': 'text/css', 'Content-Encoding': 'gzip'});
 				raw.pipe(res);
 				console.log('sent it',performance.now());
