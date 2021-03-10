dacac59a2ac918265e9fba24032c356127e327db
triplelog <robmwilc@gmail.com>
2021-03-10 08:19:20 -0500

    nm

--- static/js/curveWorker.js
+++ static/js/curveWorker.js
@@ -35,6 +35,12 @@ function sendPoints() {
 			points[2][1]+=recentPoints[i][1];
 			points[2][2]++;
 		}
+		points[0][0] /= points[0][2];
+		points[0][1] /= points[0][2];
+		points[1][0] /= points[1][2];
+		points[1][1] /= points[1][2];
+		points[2][0] /= points[2][2];
+		points[2][1] /= points[2][2];
 	}
 	else {
 		points = recentPoints;
