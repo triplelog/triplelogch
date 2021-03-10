96e623ce58239f6afcfa018589698bdd7321db08
triplelog <robmwilc@gmail.com>
2021-03-10 10:02:42 -0500

    nm

--- static/js/curveWorker.js
+++ static/js/curveWorker.js
@@ -145,7 +145,7 @@ function convexHull(points){
 		}
 	}
 	
-	var margin = 10;
+	var margin = 20;
 	cx = points[currentPoint][0];
 	cy = points[currentPoint][1];
 	hullPoints[0] = [cx,cy];
@@ -209,10 +209,14 @@ function convexHull(points){
 		if (currentPoint == -1){
 			break;
 		}
+		var d= Math.pow(Math.pow(points[currentPoint][1]-cy,2)+Math.pow(points[currentPoint][0]-cx,2),.5);
+		var adj = margin/d;
+		var mx = -1*(points[currentPoint][1]-cy)*adj;
+		var my = 1*(points[currentPoint][0]-cx)*adj;
 		cx = points[currentPoint][0];
 		cy = points[currentPoint][1];
 		hullPoints.push([cx,cy]);
-		pd += " "+cx+" "+cy;
+		pd += " "+(cx-mx)+" "+(cy-my);
 		points.splice(currentPoint,1);
 		len--;
 		
