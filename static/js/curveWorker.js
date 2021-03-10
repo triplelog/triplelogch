5aac537dee5e2584fe1949ae3437c48518ec8edf
triplelog <robmwilc@gmail.com>
2021-03-10 09:23:40 -0500

    nm

--- static/js/curveWorker.js
+++ static/js/curveWorker.js
@@ -151,7 +151,7 @@ function convexHull(points){
 	for (var i=0;i<len;i++){
 		var a = -11;
 		if (points[i][0]>cx){
-			a = Math.atan((points[i][1]-cy)/(points[i][0]-cx));
+			a = Math.atan(-1*(points[i][1]-cy)/(points[i][0]-cx));
 			toRight = true;
 		}
 		else {
